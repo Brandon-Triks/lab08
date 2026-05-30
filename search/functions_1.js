7567ce009d0bf4c78c4ee63aa264d8461bd76c58
Brandon-Triks <Brandon-Triks@users.noreply.github.com>
2026-05-30 18:36:54 +0000

    deploy: b8ff15634da25c1eac838571b7a7c0a984966746

---
+++ search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['print_0',['print',['../print_8hpp.html#ae12fd32fb711d3e888d4cd8a65974c65',1,'print(const std::string &amp;text, std::ostream &amp;out=std::cout):&#160;print.cpp'],['../print_8cpp.html#a69f26ef35f9fed4de946f282b00b4137',1,'print(const std::string &amp;text, std::ostream &amp;out):&#160;print.cpp'],['../print_8cpp.html#a88a4a55e7c5f7356f8a1fdcfbfbd1ae9',1,'print(const std::string &amp;text, std::ofstream &amp;out):&#160;print.cpp']]]
+];
